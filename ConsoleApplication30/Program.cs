cbc75c2df72c5b97d4bf479e2d0b1b79d3bf544d
GMRDL1 <gmrstudent@outlook.com>
2017-06-13 19:32:19 -0700

    intial

--- ConsoleApplication30/Program.cs
+++ ConsoleApplication30/Program.cs
@@ -10,6 +10,20 @@ namespace ConsoleApplication30
     {
         static void Main(string[] args)
         {
+            int input = int.Parse(Console.ReadLine());
+            Console.WriteLine(Factorial(input));
+            Console.ReadKey();
+        }
+        static int currentNum = 0;
+        static int counter = 0;
+        static int Factorial(int num)
+        {
+            int answer = num;
+            if (num > 1)
+            {
+                answer = num * Factorial(num - 1);
+            }
+            return answer;
         }
     }
 }
